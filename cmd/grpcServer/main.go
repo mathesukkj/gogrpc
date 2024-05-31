c9a42b6aabf23bedfedfd1e4b1dfdebd939c783f
mathesukkj <kemuel.g7363@gmail.com>
2024-05-31 19:29:49 -0300

    feat: init grpc server in main.go

---
+++ cmd/grpcServer/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net"
+
+	_ "github.com/mattn/go-sqlite3"
+	"google.golang.org/grpc"
+
+	"github.com/mathesukkj/gogrpc/internal/database"
+	"github.com/mathesukkj/gogrpc/internal/pb"
+	"github.com/mathesukkj/gogrpc/internal/services"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatalf("failed to open db: %v", err)
+	}
+	defer db.Close()
+
+	categoryDb := database.NewCategoryDb(db)
+	categoryService := services.NewCategoryService(*categoryDb)
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterCategoryServiceServer(grpcServer, categoryService)
+
+	lis, err := net.Listen("tcp", "50051")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := grpcServer.Serve(lis); err != nil {
+		panic(err)
+	}
+}
