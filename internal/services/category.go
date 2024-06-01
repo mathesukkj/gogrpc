3b3198d957004eb0d82b47d7196875e9fc906453
mathesukkj <kemuel.g7363@gmail.com>
2024-06-01 10:43:41 -0300

    feat: add CreateCategoryStream with bidirectional communication - cool isnt it

--- internal/services/category.go
+++ internal/services/category.go
@@ -100,3 +100,31 @@ func (c *CategoryService) CreateCategoryStream(
 		})
 	}
 }
+
+func (c *CategoryService) CreateCategoryStreamBidirectional(
+	stream pb.CategoryService_CreateCategoryStreamBidirectionalServer,
+) error {
+	for {
+		category, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+
+		categoryResult, err := c.CategoryDB.Create(category.Name, category.Description)
+		if err != nil {
+			return err
+		}
+
+		err = stream.Send(&pb.Category{
+			Id:          categoryResult.ID,
+			Name:        category.Name,
+			Description: category.Description,
+		})
+		if err != nil {
+			return err
+		}
+	}
+}
